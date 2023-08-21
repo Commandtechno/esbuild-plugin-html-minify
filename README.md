3b3506fd539117379dc4a6864d8d11e43fe06221
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2023-08-21 12:36:46 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Gave up on full HTML bundling that I had attempted before.
+
+Simply uses htmlnano/posthtml to minify HTML files.
+
+You can pass plugins to posthtml when initiating the plugin.
---
+++ index.js
@@ -0,0 +1,34 @@
+const { readFile } = require("fs/promises");
+
+const posthtml = require("posthtml");
+/** @type {import('htmlnano').default} */
+const htmlnano = require("htmlnano");
+
+/**
+ * @param {import('posthtml').Plugin[]} plugins
+ * @param {import('htmlnano').HtmlnanoOptions} options
+ * @return {import('esbuild').Plugin}
+ */
+module.exports = (plugins, options) => ({
+  name: "esbuild-plugin-minify-html",
+  setup(ctx) {
+    const nano = htmlnano({
+      minifyCss: false,
+      minifyJs: false,
+      minifySvg: false,
+      collapseWhitespace: "all",
+      ...options
+    });
+
+    const html = posthtml(plugins).use(nano);
+
+    ctx.onLoad({ filter: /\.html$/ }, async args => {
+      const result = await html.process(await readFile(args.path, "utf8"));
+
+      return {
+        contents: result.html,
+        loader: "copy"
+      };
+    });
+  }
+});
